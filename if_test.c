4f36900d1e2fa6e6043e56fc125e56109f6fe9d2
braillescreen <patrick@pwry.net>
2022-05-08 10:01:08 -0500

    Readme

---
+++ if_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int guess = 500;
+	int target = 1000;
+	if (guess < target) {
+		printf("Sorry, %d is too low.", guess);
+	}
+	
+	else if (guess > target) {
+		printf("Sorry, %d is too high.", guess);
+	}
+	
+	else {
+		printf("Congratulations! Right on target!");
+	}
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# learning-c
+
+Where I figure out C.
+
+I'm learning from [Learn C](https://learn-c.org).
+
+Exe's are provided. I use TCC to compile these.
+
+
+Enjoy!
